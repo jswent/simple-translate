156caa3439aa9f3edcca4eb6c0fe8c21ce7c941e
James Swent <jake@osprey21.com>
2026-02-02 11:58:43 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1 className="text-zinc-600 dark:text-zinc-400">
+        Welcome to Tauri + React
+      </h1>
+    </main>
+  );
+}
+
+export default App;
